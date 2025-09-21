f61fbe237325c2ffd96e5f4084c834964c6e6194
Doncho Bachvarov <doncho.bachvarov@gmail.com>
2025-09-21 13:09:56 +0200

    5 attempts limit implemented.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,9 +1,10 @@
 import random
 
 computer_number = random.randint(1, 100)
+attempts = 5
 
-while True:
-    guess = input("Guess the number (1-100)")
+while attempts > 0:
+    guess = input("Guess the number (1-100) in 5 attempts.")
     if not guess.isdigit():
         print("Invalid input. Try again...")
         continue
@@ -16,3 +17,6 @@ while True:
         print("Too high!")
     else:
         print("Too low!")
+
+if attempts == 0:
+    print("You lose!")
